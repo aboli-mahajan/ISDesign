0508d6aae14d97478b94eecf235751db266b86f3
Aboli Mahajan <abolimahajan23@gmail.com>
2019-03-30 20:25:41 -0500

    Testing protoype login page in flask

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ def index():
 def signup():
     return render_template('register.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
