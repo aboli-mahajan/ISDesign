80d49f5fa1c7fa0ca35110f1c98a5aebd29760bb
Aboli Mahajan <abolimahajan23@gmail.com>
2019-04-01 12:43:32 -0500

    Moved static files into respective folders. Added dummy code to add apartments. Connected home page to database. Added code to fetch file from database. Added all cities on the landing page.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
 from flask_pymongo import PyMongo
 from flask import Flask, render_template, url_for, request, session, redirect
-from dnspython import dns
 import bcrypt
+import string
+import random
 
 app = Flask(__name__)
 
@@ -11,9 +11,16 @@ app.config['MONGO_URI'] = 'mongodb+srv://demo:demo123@cluster0-kmntv.mongodb.net
 
 mongo = PyMongo(app)
 
+def generateKey():
+    return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(6))
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+    AustinApartments = fetch_apartments({'city': 'Austin'})
+    MiamiApartments = fetch_apartments({'city': 'Miami'})
+    NycApartments = fetch_apartments({'city': 'New York'})
+    SfApartments = fetch_apartments({'city': 'San Francisco'})
+    return render_template('index.html', AustinApartments=AustinApartments, MiamiApartments=MiamiApartments, NycApartments=NycApartments, SfApartments=SfApartments)
 
 @app.route('/signup')
 def signup():
@@ -33,26 +40,52 @@ def register():
             hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
             users.insert({'email': request.form['email'], 'password': hashpass})
             session['email'] = request.form['email']
-            return redirect(url_for('home'))
+            return redirect(url_for('index'))
 
         return 'That email already exists!'
 
     return render_template('register.html')
 
-@app.route('/apartments', methods=['GET'])
+@app.route('/file/<filename>')
+def file(filename):
+    return mongo.send_file(filename)
+
+@app.route('/addapartments', methods=['GET', 'POST'])
+def addapartments():
+    if request.method == 'POST':
+        request_params = {}
+        request_params['city'] = request.form['city']
+        request_params['title'] = request.form['title']
+        request_params['bedrooms'] = int(request.form['bedrooms'])
+        request_params['price_range'] = request.form['price_range']
+        if 'furnished' in request.form:
+            request_params['furnished'] = True
+        else:
+            request_params['furnished'] = False
+        request_params['apartmentKey'] = generateKey()
+
+        if 'photo' in request.files:
+            photo = request.files['photo']
+            mongo.save_file(photo.filename, photo)
+            request_params['image_name'] = photo.filename
+            mongo.db.apartments.insert(request_params)
+
+    if request.method == 'GET':
+        return render_template('add_apartments.html')
+    return render_template('index.html')
+
+@app.route('/apartments')
 def apartments():
-    # if request.method == 'GET':
-    #     request_params = {}
-    #     if 'city' in register.form:
-    #         request_params['city'] = request.form['city']
-    #     request_params['bedrooms'] = request.form['bedrooms']
-    #     request_params['price_range'] = request.form['price_range']
-    #     request_params['furnished'] = request.form['furnished']
-    #     request_params['apartmentKey'] = request.form['apartmentKey']
-    #     request_params['title'] = request.form['title']
-    #     apartmentsList = mongo.db.apartments.find(request_params)
-    return render_template('apartments.html')
+    # Does not work
+    AustinApartments = fetch_apartments({'city':'Austin'})
+    length = AustinApartments.count()
+    return render_template('apartments.html', AustinApartments=AustinApartments, length=length)
+
+def fetch_apartments(params):
+    apartmentsList = mongo.db.apartments.find(params)
+    return apartmentsList
 
 if __name__ == '__main__':
-    app.secret_key = 'mysecret1'
     app.run()
+
+app.secret_key = 'mysecret'
