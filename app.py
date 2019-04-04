77a5aed1b0633a108b11e7836503c74b2a54abbc
neeleshjayaraman <neeleshjayaraman9795@gmail.com>
2019-04-04 17:07:35 -0500

    Login file changes. Only html file

--- app.py
+++ app.py
@@ -37,8 +37,9 @@ def login():
         login_user = users.find_one({'email': request.form['username']})
 
         if login_user:
-            if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
+            if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
                 session['email'] = request.form['username']
+                
                 return redirect(url_for('index'))
         return 'invalid user'
 
